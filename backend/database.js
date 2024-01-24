dbcf130300a1c99b0ff2d26840af168e6845a849
Ezeala Abundance <136131592+Krisbethel@users.noreply.github.com>
2024-01-24 20:10:15 +0100

    Update database.js

--- backend/database.js
+++ backend/database.js
@@ -8,7 +8,7 @@ const pool = mysql.createPool({
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_DATABASE || 'theatredb',
+  database: process.env.DB_DATABASE || 'bnapgiijrssjt7p5khgt',
   handshakeTimeout: 3600000,
 });
 
